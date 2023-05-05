32281592c3a9dc50d5dd280b43f2dac3d7c83367
VaidehiSom <som.vaidehi920@gmail.com>
2023-05-05 11:06:57 -0400

    removing extra files

--- src/GVINS/src/parameters.cpp
+++ src/GVINS/src/parameters.cpp
@@ -36,6 +36,24 @@ uint32_t GNSS_TRACK_NUM_THRES;
 double GNSS_DDT_WEIGHT;
 std::string GNSS_RESULT_PATH;
 
+std::string IMAGE_TOPIC;
+std::string IMU_TOPIC;
+std::vector<std::string> CAM_NAMES;
+std::string FISHEYE_MASK;
+int MAX_CNT;
+int MIN_DIST;
+int WINDOW_SIZE;
+int FREQ;
+double F_THRESHOLD;
+int SHOW_TRACK;
+int STEREO_TRACK;
+int EQUALIZE;
+int ROW;
+int COL;
+int FOCAL_LENGTH;
+int FISHEYE;
+bool PUB_THIS_FRAME;
+
 template <typename T>
 T readParam(ros::NodeHandle &n, std::string name)
 {
@@ -69,6 +87,33 @@ void readParameters(ros::NodeHandle &n)
     MIN_PARALLAX = fsSettings["keyframe_parallax"];
     MIN_PARALLAX = MIN_PARALLAX / FOCAL_LENGTH;
 
+    // other file
+    std::string GVINS_FOLDER_PATH = readParam<std::string>(n, "gvins_folder"); // loading gvins pkg folder path
+
+    fsSettings["image_topic"] >> IMAGE_TOPIC; // reading image topic
+    fsSettings["imu_topic"] >> IMU_TOPIC; // reading imu topic
+    MAX_CNT = fsSettings["max_cnt"]; // max feature number in feature tracking
+    MIN_DIST = fsSettings["min_dist"]; // min distance between two features
+    ROW = fsSettings["image_height"];
+    COL = fsSettings["image_width"];
+    FREQ = fsSettings["freq"]; // frequence (Hz) of publish tracking result. At least 10Hz for good estimation. If set 0, the frequence will be same as raw image
+    F_THRESHOLD = fsSettings["F_threshold"]; // ransac threshold (pixel)
+    SHOW_TRACK = fsSettings["show_track"]; // publish tracking image as topic
+    EQUALIZE = fsSettings["equalize"]; // if image is too dark or light, trun on equalize to find enough features
+    FISHEYE = fsSettings["fisheye"]; // if using fisheye
+    if (FISHEYE == 1)
+        FISHEYE_MASK = GVINS_FOLDER_PATH + "config/fisheye_mask.jpg"; //! where is this file?
+    CAM_NAMES.push_back(config_file);
+
+    WINDOW_SIZE = 20;
+    STEREO_TRACK = false; //! what is this?
+    FOCAL_LENGTH = 460;
+    PUB_THIS_FRAME = false;
+
+    if (FREQ == 0)
+        FREQ = 100;
+    // other file
+    
     std::string tmp_output_dir;
     fsSettings["output_dir"] >> tmp_output_dir;
     assert(!tmp_output_dir.empty() && "Output directory cannot be empty.\n");
